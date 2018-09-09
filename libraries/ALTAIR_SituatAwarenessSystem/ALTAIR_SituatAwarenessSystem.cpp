21d201ab1309ce61c81f196239ef348630236d2a
jalbert10NJ <jalbert@uvic.ca>
2018-09-09 14:58:39 -0700

    Add files via upload

---
+++ libraries/ALTAIR_SituatAwarenessSystem/ALTAIR_SituatAwarenessSystem.cpp
@@ -0,0 +1,65 @@
+/**************************************************************************/
+/*!
+    @file     ALTAIR_SituatAwarenessSystem.cpp
+    @author   Justin Albert (jalbert@uvic.ca)
+    @license  GPL
+
+    This is the class for the ALTAIR "situational awareness" system, which
+    includes the two GPS receivers (the NEO-M8N on the mast, and the
+    DFRobot G6 in a small plastic housing directly atop the payload); the
+    three orientation sensors (the Adafruit BNO055 accel/gyro/mag on the
+    mast, the CHRobotics UM7 accel/gyro/mag within the payload, and the
+    SparkFun HMC6343 accel/mag on the mast); the three Adafruit BME280
+    pressure/temp/humidity sensors; and the many (primarily LM333) other
+    temperature sensors located at various places within and around ALTAIR.
+
+    This class is instantiated as a singleton via the instantiation of the
+    (also singleton) ALTAIR_GlobalDeviceControl class.
+
+    Justin Albert  jalbert@uvic.ca     began on 5 Sept. 2018
+
+    @section  HISTORY
+
+    v1.0  - First release
+*/
+/**************************************************************************/
+
+#include "ALTAIR_SituatAwarenessSystem.h"
+
+/**************************************************************************/
+/*!
+ @brief  Constructor.  
+*/
+/**************************************************************************/
+ALTAIR_SituatAwarenessSystem::ALTAIR_SituatAwarenessSystem()
+{
+}
+
+/**************************************************************************/
+/*!
+ @brief  Initialize all devices.
+*/
+/**************************************************************************/
+void ALTAIR_SituatAwarenessSystem::initialize(             )
+{
+    _orientSensors.initialize() ;
+
+     Serial.println(F("BME280 pressure/temp/humidity sensors initialization..."));
+     bool status1, status2, status3, statusall;    
+     status1 = _bmeMast.begin();
+     status2 = _bmeBalloon.begin(0x76);
+//     tcaselect(0);
+//     status3 = bmePayload.begin();
+//     tcaselect(-1);
+     statusall = status1 && status3; // && status2;
+     if (!statusall) {
+         Serial.print(F("Could not find one of the 3 BME280 sensors, check wiring!  BME280 on nav mast: ")); Serial.print(status1); 
+         Serial.print(F("   BME280 in the balloon valve: ")); Serial.print(status2); 
+         Serial.print(F("   BME280 inside the gondola: ")); Serial.println(status3); 
+         while (1);
+     }
+
+    _gpsSensors.initialize() ;
+}
+
+
--- libraries/ALTAIR_SituatAwarenessSystem/ALTAIR_SituatAwarenessSystem.h
+++ libraries/ALTAIR_SituatAwarenessSystem/ALTAIR_SituatAwarenessSystem.h
@@ -1,1 +1,66 @@
+/**************************************************************************/
+/*!
+    @file     ALTAIR_SituatAwarenessSystem.h
+    @author   Justin Albert (jalbert@uvic.ca)
+    @license  GPL
 
+    This is the class for the ALTAIR "situational awareness" system, which
+    includes the two GPS receivers (the NEO-M8N on the mast, and the 
+    DFRobot G6 in a small plastic housing directly atop the payload); the 
+    three orientation sensors (the Adafruit BNO055 accel/gyro/mag on the 
+    mast, the CHRobotics UM7 accel/gyro/mag within the payload, and the
+    SparkFun HMC6343 accel/mag on the mast); the three Adafruit BME280 
+    pressure/temp/humidity sensors; and the many (primarily LM333) other
+    temperature sensors located at various places within and around ALTAIR.
+
+    This class is instantiated as a singleton via the instantiation of the
+    (also singleton) ALTAIR_GlobalDeviceControl class.
+
+    Justin Albert  jalbert@uvic.ca     began on 5 Sept. 2018
+
+    @section  HISTORY
+
+    v1.0  - First version
+*/
+/**************************************************************************/
+
+#ifndef ALTAIR_SituatAwarenessSystem_h
+#define ALTAIR_SituatAwarenessSystem_h
+
+#include "Arduino.h"
+#include "ALTAIR_GPSSensors.h"
+#include "ALTAIR_OrientSensors.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+class ALTAIR_SituatAwarenessSystem {
+  public:
+
+    ALTAIR_SituatAwarenessSystem(                        )                                    ;
+
+    ALTAIR_GPSSensors&       gpsSensors(                 ) { return _gpsSensors               ; }
+
+    ALTAIR_OrientSensors&    orientSensors(              ) { return _orientSensors            ; }
+
+    Adafruit_BME280&         bmeMast(                    ) { return _bmeMast                  ; }
+    Adafruit_BME280&         bmeBalloon(                 ) { return _bmeBalloon               ; }
+    Adafruit_BME280&         bmePayload(                 ) { return _bmePayload               ; }
+
+    void                     initialize(                 )                                    ;
+    void                     switchToOtherGPS(           ) { _gpsSensors.switchToOtherGPS()   ; }
+    void                     switchToBackupOrientSensor1() { _orientSensors.switchToBackup1() ; }
+    void                     switchToBackupOrientSensor2() { _orientSensors.switchToBackup2() ; }
+
+  protected:
+
+  private:
+    ALTAIR_GPSSensors        _gpsSensors                                                      ;
+
+    ALTAIR_OrientSensors     _orientSensors                                                   ;
+
+    Adafruit_BME280          _bmeMast                                                         ;
+    Adafruit_BME280          _bmeBalloon                                                      ;
+    Adafruit_BME280          _bmePayload                                                      ;
+
+};
+#endif    //   ifndef ALTAIR_SituatAwarenessSystem_h
