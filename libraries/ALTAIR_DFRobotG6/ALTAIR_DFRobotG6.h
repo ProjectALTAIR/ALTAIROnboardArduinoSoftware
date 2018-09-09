9a762abf36403260b3014da5c52c161fb664450a
jalbert10NJ <jalbert@uvic.ca>
2018-09-09 15:05:40 -0700

    Add files via upload

--- libraries/ALTAIR_DFRobotG6/ALTAIR_DFRobotG6.h
+++ libraries/ALTAIR_DFRobotG6/ALTAIR_DFRobotG6.h
@@ -1,1 +1,34 @@
+/**************************************************************************/
+/*!
+    @file     ALTAIR_DFRobotG6.h
+    @author   Justin Albert (jalbert@uvic.ca)
+    @license  GPL
 
+    This is the class for the ALTAIR DFRobot G6 GPS receiver, located in
+
+
+    Justin Albert  jalbert@uvic.ca     began on 6 Sep. 2018
+
+    @section  HISTORY
+
+    v1.0  - First version
+*/
+/**************************************************************************/
+
+#ifndef ALTAIR_DFRobotG6_h
+#define ALTAIR_DFRobotG6_h
+
+#include "Arduino.h"
+#include "ALTAIR_GPSSensor.h"
+
+class ALTAIR_DFRobotG6 : public ALTAIR_GPSSensor {
+  public:
+
+    ALTAIR_DFRobotG6(                )           {  }
+
+    virtual void         initialize( )           {  }
+
+  private:
+
+};
+#endif    //   ifndef ALTAIR_DFRobotG6_h
