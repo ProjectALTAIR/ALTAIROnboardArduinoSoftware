9f40d96b4511c86c33030c09faeb05976746b828
jalbert10NJ <jalbert@uvic.ca>
2018-09-09 14:53:40 -0700

    Add files via upload

---
+++ libraries/ALTAIR_LightSourceMonitoring/ALTAIR_LightSourceMonitoring.cpp
@@ -0,0 +1,47 @@
+/**************************************************************************/
+/*!
+    @file     ALTAIR_LightSourceMonitoring.cpp
+    @author   Justin Albert (jalbert@uvic.ca)
+    @license  GPL
+
+    This class contains methods for initializing and controlling all
+    ALTAIR light source monitoring electronics.  This includes the 
+    photodiode amplifier boards and the ADC boards (and their associated 
+    environmental monitoring).
+
+    Justin Albert  jalbert@uvic.ca     began on 8 Sep. 2018
+
+    @section  HISTORY
+
+    v1.0  - First version
+*/
+/**************************************************************************/
+
+#include "ALTAIR_LightSourceMonitoring.h"
+
+/**************************************************************************/
+/*!
+ @brief  Default constructor.
+*/
+/**************************************************************************/
+ALTAIR_LightSourceMonitoring::ALTAIR_LightSourceMonitoring(  ) :
+   _ads1115ADC1(              DEFAULT_ADS1115ADC_I2CADDRESS  ) ,
+   _ads1115ADC2(              DEFAULT_ADS1115ADC2_I2CADDRESS ) ,
+   _ads1115ADC3(              DEFAULT_ADS1115ADC3_I2CADDRESS )
+{
+
+}
+
+/**************************************************************************/
+/*!
+ @brief  Initialize the three ADS1115 boards (within the setup routine).
+*/
+/**************************************************************************/
+void ALTAIR_LightSourceMonitoring::initialize(               )
+{
+    Serial.println(F("Initializing the three ADS1115 4-channel ADC breakout boards ..."));
+   _ads1115ADC1.begin();
+   _ads1115ADC2.begin();
+   _ads1115ADC3.begin();
+}
+
--- libraries/ALTAIR_LightSourceMonitoring/ALTAIR_LightSourceMonitoring.h
+++ libraries/ALTAIR_LightSourceMonitoring/ALTAIR_LightSourceMonitoring.h
@@ -1,1 +1,49 @@
+/**************************************************************************/
+/*!
+    @file     ALTAIR_LightSourceMonitoring.h
+    @author   Justin Albert (jalbert@uvic.ca)
+    @license  GPL
+
+    This class contains methods for initializing and controlling all
+    ALTAIR light source monitoring electronics.  This includes the 
+    photodiode amplifier boards and the ADC boards (and their associated 
+    environmental monitoring).
+
+    Justin Albert  jalbert@uvic.ca     began on 8 Sep. 2018
+
+    @section  HISTORY
+
+    v1.0  - First version
+*/
+/**************************************************************************/
+
+#ifndef   ALTAIR_LightSourceMonitoring_h
+#define   ALTAIR_LightSourceMonitoring_h
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADS1015.h>    // is the proper header for the Adafruit ADS 1115 ADCs (as well)
+
+#define   DEFAULT_ADS1115ADC_I2CADDRESS               0x48
+#define   DEFAULT_ADS1115ADC2_I2CADDRESS              0x49
+#define   DEFAULT_ADS1115ADC3_I2CADDRESS              0x4A
+
+class ALTAIR_LightSourceMonitoring {
+  public:
+    virtual void         initialize(  )                           ;
+
+    Adafruit_ADS1115&    ads1115ADC1( )    { return  _ads1115ADC1 ; }
+    Adafruit_ADS1115&    ads1115ADC2( )    { return  _ads1115ADC2 ; }
+    Adafruit_ADS1115&    ads1115ADC3( )    { return  _ads1115ADC3 ; }
+
+    ALTAIR_LightSourceMonitoring(     )                           ;
+
+  protected:
+
+  private:
+
+    Adafruit_ADS1115    _ads1115ADC1                              ;
+    Adafruit_ADS1115    _ads1115ADC2                              ;
+    Adafruit_ADS1115    _ads1115ADC3                              ;
+};
+#endif    //   ifndef ALTAIR_LightSourceMonitoring_h
 
